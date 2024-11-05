876596dfd564e5fa2eef2bbb590443a311131e0a
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2024-11-05 09:23:59 +0000

    Add Websocket documentation

--- sample.mjs
+++ sample.mjs
@@ -1,4 +1,5 @@
-import { neon, neonConfig } from '@neondatabase/serverless';
+import { neon, neonConfig, Pool } from '@neondatabase/serverless';
+import ws from 'ws';
 
 const connectionString = 'postgres://postgres:postgres@db.localtest.me:5432/main';
 
@@ -6,6 +7,17 @@ neonConfig.fetchEndpoint = (host) => {
   const [protocol, port] = host === 'db.localtest.me' ? ['http', 4444] : ['https', 443];
   return `${protocol}://${host}:${port}/sql`;
 };
+
+neonConfig.wsProxy = (host) => `${host}:4444/v1`;
+neonConfig.useSecureWebSocket = false;
+neonConfig.pipelineTLS = false;
+neonConfig.pipelineConnect = false;
+neonConfig.webSocketConstructor = ws;
+
+const pool = new Pool({ connectionString });
+const { rows } = await pool.query('SELECT * FROM NOW()');
+
+console.log(rows[0]);
 
 const sql = neon(connectionString);
 const [result] = await sql`SELECT * FROM NOW()`;
